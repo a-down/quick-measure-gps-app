98a003630e3a11f76146f14782db8a6e89af2375
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-08 16:28:41 -0600

    build attempt

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-blue-200 text-xl font-bold">Open up App.jsss to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}", 
+    "./screens/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
