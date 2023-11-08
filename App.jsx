76d85b092f0e229b9dad3ae899205cdaa8bde974
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-08 16:38:59 -0600

    MapView and Polygon Setup

--- App.jsx
+++ App.jsx
@@ -1,13 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
+import MapView, { Polygon } from 'react-native-maps';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text className="text-blue-200 text-xl font-bold">Open up App.jsss to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView 
+        style={{flex: 1, width: '100%'}}
+        initialRegion={{
+          latitude: 44.010862,
+          longitude: -92.411936,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+        }}>
+        <Polygon 
+          strokeColor="red"
+          strokeWidth={2}
+          coordinates={[
+            {
+              latitude: 44.010862,
+              longitude: -92.411936
+            },
+            {
+              latitude: 44.011397,
+              longitude: -92.405360
+            },
+            {
+              latitude: 44.007199,
+              longitude: -92.404200
+            },
+            {
+              latitude: 44.008040,
+              longitude: -92.410658
+            }
+          ]}
+        />
+      </MapView>
     </View>
   );
 }
