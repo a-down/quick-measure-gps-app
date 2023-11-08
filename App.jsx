809db1b60af26f04d26238531a75c70c0535c117
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-08 16:57:59 -0600

    expo-location

--- App.jsx
+++ App.jsx
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
-import MapView, { Polygon } from 'react-native-maps';
+import MapView, { Polygon, Marker } from 'react-native-maps';
+import { useEffect, useState} from 'react';
+import * as Location from "expo-location";
+
 
 
 export default function App() {
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [initialRegion, setInitialRegion] = useState(null);
+
+  useEffect(() => {
+    const getLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setCurrentLocation(location.coords);
+
+      setInitialRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      });
+    };
+
+    getLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
+      <View style={{flex: 0.2, backgroundColor: '#fff', justifyContent: 'flex-end', alignItems: 'center'}}>
+        <View style={{flex: 1, justifyContent: 'flex-end'}}>
+          <Text style={{color: '#000', fontSize: 24}}>Map</Text>
+          <Text style={{color: '#000', fontSize: 24}}>Map</Text>
+          <Text style={{color: '#000', fontSize: 24}}>Map</Text>
+        </View>
+      </View>
       <MapView 
         style={{flex: 1, width: '100%'}}
         initialRegion={{
-          latitude: 44.010862,
-          longitude: -92.411936,
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
           latitudeDelta: 0.01,
           longitudeDelta: 0.01
         }}>
+          {currentLocation && (
+            <Marker
+              coordinate={{
+                latitude: currentLocation ? currentLocation.latitude : 0,
+                longitude: currentLocation.longitude,
+              }}
+              title="Your Location"
+            />
+          )}
         <Polygon 
           strokeColor="red"
           strokeWidth={2}
