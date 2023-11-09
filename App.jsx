40167ce20c693416d8529f20ce8d0f9a078b3049
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-09 12:53:35 -0600

    add new location to polygon

--- App.jsx
+++ App.jsx
@@ -1,18 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 import MapView, { Polygon, Marker } from 'react-native-maps';
 import { useEffect, useState} from 'react';
 import * as Location from "expo-location";
 
 
+const demoCoordinates = [
+  {
+    latitude: 44.007199,
+    longitude: -92.404200
+  },
+  {
+    latitude: 44.008040,
+    longitude: -92.410658
+  },
+  {
+    latitude: 44.010862,
+    longitude: -92.411936
+  },
+  {
+    latitude: 44.011397,
+    longitude: -92.405360
+  },
+]
+
 
 export default function App() {
-  const [currentLocation, setCurrentLocation] = useState(null);
-  const [initialRegion, setInitialRegion] = useState(null);
+  const [ currentLocation, setCurrentLocation ] = useState(null);
+  const [ initialRegion, setInitialRegion ] = useState(null);
+  const [ polygonCoordinates, setPolygonCoordinates ] = useState(demoCoordinates)
 
   useEffect(() => {
-    const getLocation = async () => {
+    const getInitialLocation = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         console.log("Permission to access location was denied");
@@ -31,60 +51,65 @@ export default function App() {
       });
     };
 
-    getLocation();
+    getInitialLocation();
   }, []);
+
+  const addLocationToPolygon = async () => {
+    let location = await Location.getCurrentPositionAsync({});
+    await setCurrentLocation(location.coords);
+    const newLocation = location.coords
+    await setPolygonCoordinates([newLocation, ...polygonCoordinates])
+    console.log(location.coords.latitude, location.coords.longitude)
+    console.log(polygonCoordinates)
+  }
 
   return (
     <View style={styles.container}>
       <View style={{flex: 0.2, backgroundColor: '#fff', justifyContent: 'flex-end', alignItems: 'center'}}>
         <View style={{flex: 1, justifyContent: 'flex-end'}}>
-          <Text style={{color: '#000', fontSize: 24}}>Map</Text>
-          <Text style={{color: '#000', fontSize: 24}}>Map</Text>
-          <Text style={{color: '#000', fontSize: 24}}>Map</Text>
+          {currentLocation && (
+            <>
+              <Text style={{color: '#000', fontSize: 24}}>{`lat: ${currentLocation.latitude}`}</Text>
+              <Text style={{color: '#000', fontSize: 24}}>{`long: ${currentLocation.longitude}`}</Text>
+            </>
+          )}
+
         </View>
       </View>
+
       {initialRegion && (
-      <MapView 
-        style={{flex: 1, width: '100%'}}
-        initialRegion={{
-          latitude: initialRegion.latitude,
-          longitude: initialRegion.longitude,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01
-        }}>
-          {currentLocation && (
-            <Marker
-              coordinate={{
-                latitude: currentLocation.latitude,
-                longitude: currentLocation.longitude,
-              }}
-              title="Your Location"
+        <MapView 
+          style={{flex: 1, width: '100%'}}
+          initialRegion={{
+            latitude: initialRegion.latitude,
+            longitude: initialRegion.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01
+          }}>
+
+            {currentLocation && (
+              <Marker
+                coordinate={{
+                  latitude: currentLocation.latitude,
+                  longitude: currentLocation.longitude,
+                }}
+                title="Your Location"
+              />
+            )}
+
+            <Polygon 
+              strokeColor="red"
+              strokeWidth={2}
+              coordinates={polygonCoordinates}
             />
-          )}
-        <Polygon 
-          strokeColor="red"
-          strokeWidth={2}
-          coordinates={[
-            {
-              latitude: 44.010862,
-              longitude: -92.411936
-            },
-            {
-              latitude: 44.011397,
-              longitude: -92.405360
-            },
-            {
-              latitude: 44.007199,
-              longitude: -92.404200
-            },
-            {
-              latitude: 44.008040,
-              longitude: -92.410658
-            }
-          ]}
-        />
-      </MapView>
+        </MapView>
       )}
+
+      <Pressable onPress={addLocationToPolygon} style={{backgroundColor: '#92e1c0', padding: 8, position: 'absolute', bottom: 20}}>
+        <Text>
+          Add Location
+        </Text>
+      </Pressable>
     </View>
   );
 }
