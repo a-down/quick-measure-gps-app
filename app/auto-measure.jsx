2c1e2c76bdd5cfcb610452d5ea31ff2485a858dc
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-11 21:44:52 -0600

    alert if permission services are not enabled

--- app/auto-measure.jsx
+++ app/auto-measure.jsx
@@ -1,52 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable, useWindowDimensions, Alert } from 'react-native';
-import * as SplashScreen from 'expo-splash-screen';
+import { Text, View, Pressable, useWindowDimensions, Alert } from 'react-native';
 import MapView, { Polygon, Marker } from 'react-native-maps';
 import { useEffect, useState } from 'react';
 import * as Location from "expo-location";
 import { getAreaOfPolygon, convertArea, getPathLength } from 'geolib';
+import { useRouter, Link } from 'expo-router';
 
-const demoCoordinates = [
-  {
-    latitude: 44.007199,
-    longitude: -92.404200
-  },
-  {
-    latitude: 44.008040,
-    longitude: -92.410658
-  },
-  {
-    latitude: 44.010862,
-    longitude: -92.411936
-  },
-  {
-    latitude: 44.011397,
-    longitude: -92.405360
-  },
-]
 
 const walkToMailbox = [{latitude: 44.00719339068559, longitude: -92.39045458757248}, {latitude: 44.00720777521759, longitude: -92.39044857257788}, {latitude: 44.00722463996818, longitude: -92.39044552876923}, {latitude: 44.00723910893775, longitude: -92.39043884259915}, {latitude: 44.007253440055344, longitude: -92.3904339617919}, {latitude: 44.00726996411364, longitude: -92.39043368123015}, {latitude: 44.00728242210206, longitude: -92.39042937761312}, {latitude: 44.00729738115168, longitude: -92.39042271172833}, {latitude: 44.00730698411163, longitude: -92.39041823226454}, {latitude: 44.00731678282986, longitude: -92.39041522381036}, {latitude: 44.007331483445654, longitude: -92.39041748500719}, {latitude: 44.00734617151441, longitude: -92.3904142248112}, {latitude: 44.00735833376541, longitude: -92.39039820105242}, {latitude: 44.007364923916036, longitude: -92.39038508187748}, {latitude: 44.007367904436194, longitude: -92.39036323363482}, {latitude: 44.00737559615935, longitude: -92.39032280977409}, {latitude: 44.007378468563495, longitude: -92.39030045648173}]
 
-
 export default function AutoMeasure() {
+  const router = useRouter();
+
   const { height, width } = useWindowDimensions();
   const [ currentLocation, setCurrentLocation ] = useState(null);
   const [ initialRegion, setInitialRegion ] = useState(null);
   const [ polygonCoordinates, setPolygonCoordinates ] = useState([])
   const [ polygonArea, setPolygonArea ] = useState()
-  const [ measurementPreferences, setMeasurementPreferences ] = useState({area: 'sq meters', areaShort: 'sqm', distance: 'meters', distanceShort: 'm'})
   const [ polygonDistance, setPolygonDistance ] = useState()
+  const [ measurementPreferences, setMeasurementPreferences ] = useState({area: 'sq meters', areaShort: 'sqm', distance: 'meters', distanceShort: 'm'})
   const [ isMeasuring, setIsMeasuring ] = useState(true)
 
-  let locationSubscription = null
-
+  // check if location permission is granted
   useEffect(() => {
     const getInitialLocation = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
-        console.log("Permission to access location was denied");
+        Alert.alert(
+          "Location Permission Required",
+          "Please enable location services in your phone settings to use this feature.",
+          [
+            { text: "Go Back", style: "cancel", onPress: () => router.back()}
+          ]
+        )
         return;
-      }
+      } 
 
       let location = await Location.getCurrentPositionAsync({});
       setCurrentLocation(location.coords);
@@ -58,14 +45,13 @@ export default function AutoMeasure() {
         longitudeDelta: 0.005,
       });
 
-      locationSubscription = await Location.watchPositionAsync(
+      const locationSubscription = await Location.watchPositionAsync(
         {accuracy:Location.Accuracy.BestForNavigation, distanceInterval: 1},
         (loc) => {
           setCurrentLocation(loc.coords)
         }
       );
     };
-
     getInitialLocation();
   }, []);
 
