ab3d90e0381ad06b31757dc6149abb0160d5ca19
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-20 15:20:18 -0600

    saved measurements page shows all map info

--- app/(tabs)/saved.jsx
+++ app/(tabs)/saved.jsx
@@ -45,21 +45,21 @@ const Card = ({ item }) => {
   return (
     <View className="flex w-full bg-white relative p-4" style={{gap: 8}}>
       <View className="bg-white w-full">
-        <Text className="text-lg font-semibold text-green-10">{item.mapName}</Text>
+        <Text className="text-2xl font-semibold text-green-10">{item.mapName}</Text>
         <Text className="text-base text-gray-7">{item.dateCreated.split("T")[0]}</Text>
       </View>
 
       <View className="flex-row flex-wrap mb-4" style={{gapY: 8}}>
-        <Text className="text-lg mr-8">
-          <Text className="text-3xl">
+        <Text className="text-lg text-gray-8 mr-8">
+          <Text className="text-2xl">
             { polygonArea
               ? convertArea(polygonArea, item.measurements.areaShort).toFixed(2)
               : 0}
           </Text>
           {` `}{ item.measurements.area }
         </Text>
-        <Text className="text-lg">
-          <Text className="text-3xl">
+        <Text className="text-lg text-gray-8">
+          <Text className="text-2xl">
             { polygonDistance
               ? convertDistance(polygonDistance, item.measurements.distanceShort).toFixed(2)
               : 0 }
@@ -68,14 +68,16 @@ const Card = ({ item }) => {
         </Text>
       </View>  
 
-      <View>
-        <Pressable className="bg-green-5 p-2 rounded-md mb-2">
+      <View className=" flex-row w-full" style={{gap: 8}}>
+        <Pressable className="bg-green-5 p-2 rounded-md flex-grow">
           <Text className="text-white font-semibold text-lg text-center">View Map</Text>
         </Pressable>
-        <Button 
+        {/* <Button 
+          style={{margin: 0, padding: 0}}
           title="Delete"
           color="#C7504B"
-          onPress={() => {Alert.alert("delete")}} />
+          onPress={() => {Alert.alert("delete")}} /> */}
+        <Pressable className="bg-[#C7504B] opacity-60 w-11 h-full rounded-md" onPress={() => Alert.alert("delete")}></Pressable>
       </View>      
     </View>
   )
