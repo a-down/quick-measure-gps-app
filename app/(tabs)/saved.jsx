92d16c02a594dc484aa55924a136231f2d5004fe
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-20 15:50:12 -0600

    delete map feature is live, saved tab updates on focus

--- app/(tabs)/saved.jsx
+++ app/(tabs)/saved.jsx
@@ -1,8 +1,8 @@
 import { View, Text, Pressable, FlatList, Button, Alert } from 'react-native';
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getAreaOfPolygon, getPathLength, convertArea, convertDistance } from 'geolib';
-import { useRouter } from 'expo-router';
+import { useRouter, useFocusEffect } from 'expo-router';
 
 
 const Saved = () => {
@@ -21,68 +21,80 @@ const Saved = () => {
     }
   }
 
-  useEffect(() => {
-    getMaps()
-      // try {
-      //     AsyncStorage.removeItem('savedMaps');
-      // }
-      // catch(exception) {
-      // }
-  }, [])
+  useFocusEffect(
+    useCallback(() => {
+      getMaps()
+    }, [])
+  );
 
-//   <View className="bg-gray-2 shadow-sm">
-//   <Pressable className="flex w-full bg-white" onPress={() => router.push("/")}>
-//     <View className="bg-white w-full p-2 absolute bottom-0">
-//       <Text className="text-lg font-semibold text-green-10">{map.mapName}</Text>
-//       <Text className="text-base text-gray-7">{map.dateCreated}</Text>
-//     </View>
-//   </Pressable>
-// </View>
+  const deleteMapAlert = ({mapName, id}) => {
+    Alert.alert(
+      "Delete Map",
+      `Are you sure you want to delete ${mapName}?`,
+      [
+        { text: `Delete ${mapName}`, style: "destructive", onPress: () => deleteMap(id) },
+        { text: "Cancel", style: "cancel" }
+      ]
+    )
+  }
 
-const Card = ({ item }) => {
-  const polygonArea = getAreaOfPolygon(item.polygonCoordinates)
-  const polygonDistance = getPathLength(item.polygonCoordinates)
+  const deleteMap = async (id) => {
+    try {
+      const value = await AsyncStorage.getItem('savedMaps')
+      if (value !== null) {
+        const data = JSON.parse(value).filter(map => map.id !== id)
+        await AsyncStorage.setItem(
+          'savedMaps',
+          JSON.stringify(data)
+        );
+        setSavedMaps(data)
+        Alert.alert("Map deleted")
+      }
+    } catch (error) {
+        console.log(error)
+        Alert.alert("Could not delete map")
+    }
+  }
 
-  return (
-    <View className="flex w-full bg-white relative p-4" style={{gap: 8}}>
-      <View className="bg-white w-full">
-        <Text className="text-2xl font-semibold text-green-10">{item.mapName}</Text>
-        <Text className="text-base text-gray-7">{item.dateCreated.split("T")[0]}</Text>
+  const Card = ({ item }) => {
+    const polygonArea = getAreaOfPolygon(item.polygonCoordinates)
+    const polygonDistance = getPathLength(item.polygonCoordinates)
+
+    return (
+      <View className="flex w-full bg-white relative p-4" style={{gap: 8}}>
+        <View className="bg-white w-full">
+          <Text className="text-2xl font-semibold text-green-10">{item.mapName}</Text>
+          <Text className="text-base text-gray-7">{item.dateCreated.split("T")[0]}</Text>
+        </View>
+
+        <View className="flex-row flex-wrap mb-4" style={{gapY: 8}}>
+          <Text className="text-lg text-gray-8 mr-8">
+            <Text className="text-2xl">
+              { polygonArea
+                ? convertArea(polygonArea, item.measurements.areaShort).toFixed(2)
+                : 0}
+            </Text>
+            {` `}{ item.measurements.area }
+          </Text>
+          <Text className="text-lg text-gray-8">
+            <Text className="text-2xl">
+              { polygonDistance
+                ? convertDistance(polygonDistance, item.measurements.distanceShort).toFixed(2)
+                : 0 }
+            </Text>
+            {` `}{ item.measurements.distance }
+          </Text>
+        </View>  
+
+        <View className=" flex-row w-full" style={{gap: 8}}>
+          <Pressable className="bg-green-5 p-2 rounded-md flex-grow">
+            <Text className="text-white font-semibold text-lg text-center">View Map</Text>
+          </Pressable>
+          <Pressable className="bg-[#C7504B] opacity-60 w-11 h-full rounded-md" onPress={() => deleteMapAlert({id: item.id, mapName: item.mapName})}></Pressable>
+        </View>      
       </View>
-
-      <View className="flex-row flex-wrap mb-4" style={{gapY: 8}}>
-        <Text className="text-lg text-gray-8 mr-8">
-          <Text className="text-2xl">
-            { polygonArea
-              ? convertArea(polygonArea, item.measurements.areaShort).toFixed(2)
-              : 0}
-          </Text>
-          {` `}{ item.measurements.area }
-        </Text>
-        <Text className="text-lg text-gray-8">
-          <Text className="text-2xl">
-            { polygonDistance
-              ? convertDistance(polygonDistance, item.measurements.distanceShort).toFixed(2)
-              : 0 }
-          </Text>
-          {` `}{ item.measurements.distance }
-        </Text>
-      </View>  
-
-      <View className=" flex-row w-full" style={{gap: 8}}>
-        <Pressable className="bg-green-5 p-2 rounded-md flex-grow">
-          <Text className="text-white font-semibold text-lg text-center">View Map</Text>
-        </Pressable>
-        {/* <Button 
-          style={{margin: 0, padding: 0}}
-          title="Delete"
-          color="#C7504B"
-          onPress={() => {Alert.alert("delete")}} /> */}
-        <Pressable className="bg-[#C7504B] opacity-60 w-11 h-full rounded-md" onPress={() => Alert.alert("delete")}></Pressable>
-      </View>      
-    </View>
-  )
-}
+    )
+  }
 
   return (
     <View className=" bg-gray-1 flex-1">
@@ -94,13 +106,6 @@ const Card = ({ item }) => {
           ItemSeparatorComponent={() => <View className="h-4 bg-gray-1"></View>}
           />
       )}
-      <Button
-        title="Clear Saved Maps"
-        onPress={() => AsyncStorage.removeItem('savedMaps')} />
-      <Button
-        title="Refresh Maps"
-        onPress={() => setSavedMaps(AsyncStorage.getItem('savedMaps'))} />
-
     </View>
   )
 }
