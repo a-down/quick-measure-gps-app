4bf3607854eeaa31b3a2487921afb4943e579a9c
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-22 22:35:24 -0600

    AutoMeasure saves current coordinates to memory

--- app/AutoMeasureScreen.jsx
+++ app/AutoMeasureScreen.jsx
@@ -6,6 +6,7 @@ import { getAreaOfPolygon, getPathLength } from 'geolib';
 import { useRouter } from 'expo-router';
 import { MeasurementDisplay, StopMeasuringButton, ResetMeasurementsButton, SaveMeasurementsButton } from '../components';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useStorage } from '../hooks';
 
 const walkToMailbox = [{latitude: 44.00719339068559, longitude: -92.39045458757248}, {latitude: 44.00720777521759, longitude: -92.39044857257788}, {latitude: 44.00722463996818, longitude: -92.39044552876923}, {latitude: 44.00723910893775, longitude: -92.39043884259915}, {latitude: 44.007253440055344, longitude: -92.3904339617919}, {latitude: 44.00726996411364, longitude: -92.39043368123015}, {latitude: 44.00728242210206, longitude: -92.39042937761312}, {latitude: 44.00729738115168, longitude: -92.39042271172833}, {latitude: 44.00730698411163, longitude: -92.39041823226454}, {latitude: 44.00731678282986, longitude: -92.39041522381036}, {latitude: 44.007331483445654, longitude: -92.39041748500719}, {latitude: 44.00734617151441, longitude: -92.3904142248112}, {latitude: 44.00735833376541, longitude: -92.39039820105242}, {latitude: 44.007364923916036, longitude: -92.39038508187748}, {latitude: 44.007367904436194, longitude: -92.39036323363482}, {latitude: 44.00737559615935, longitude: -92.39032280977409}, {latitude: 44.007378468563495, longitude: -92.39030045648173}]
 
@@ -14,7 +15,7 @@ export default function AutoMeasure() {
 
   const [ currentLocation, setCurrentLocation ] = useState(null);
   // const [ region, setRegion ] = useState(null);
-  const [ polygonCoordinates, setPolygonCoordinates ] = useState(walkToMailbox)
+  const [ polygonCoordinates, setPolygonCoordinates ] = useState([])
   const [ polygonArea, setPolygonArea ] = useState()
   const [ polygonDistance, setPolygonDistance ] = useState()
   const [ isMeasuring, setIsMeasuring ] = useState(true)
@@ -25,7 +26,10 @@ export default function AutoMeasure() {
     // if so, set initial region as current location
     // if so, start locationSubscription
   useEffect(() => {
-    getMapPreferences()
+    useStorage('get', 'mapPreferences').then(value => setMapType(value))
+    useStorage('get', 'currentAutoCoordinates').then(value => {
+      if (value) setPolygonCoordinates(value)
+    })
     const getInitialLocation = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
@@ -56,23 +60,13 @@ export default function AutoMeasure() {
   useEffect(() => {
     if (currentLocation && isMeasuring) {
       addLocationToPolygon(currentLocation)
+      useStorage('set', 'currentAutoCoordinates', polygonCoordinates)
     }
     if (polygonCoordinates.length > 1 && isMeasuring) {
       setPolygonDistance(getPathLength(polygonCoordinates))
       setPolygonArea(getAreaOfPolygon(polygonCoordinates))
     }
   }, [currentLocation])
-
-  const getMapPreferences = async () => {
-    try {
-      const value = await AsyncStorage.getItem('mapPreferences');
-      if (value !== null) {
-        setMapType(JSON.parse(value))
-      } 
-    } catch (error) {
-        console.log(error)
-    }
-  }
 
   // add a new location to the polygon
   const addLocationToPolygon = async (newLocation) => {
