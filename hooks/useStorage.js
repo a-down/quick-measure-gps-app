c9116f24f1af7260f6d0694b52360d15603ab9b6
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-22 23:40:12 -0600

    added error handling for useStorage

--- hooks/useStorage.js
+++ hooks/useStorage.js
@@ -1,6 +1,12 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
 export default useStorage = async (method, itemName, data) => {
+  if (method !== "set" && method !== "get" && method !== "remove") console.warn(`"${method}" is not a valid argument for useStorage. Please use "get", "set", or "remove" as the first argument.`)
+
+  if (!method) console.warn(`useStorage requires a method as the first argument. Please use "get", "set", or "remove" as the first argument.`)
+
+  if (!itemName) console.warn(`useStorage requires an itemName. Please include the key of the item you wish to access as the second argument.`)
+
   try {
     switch (method) {
       case ('get'):
@@ -8,13 +14,12 @@ export default useStorage = async (method, itemName, data) => {
         return JSON.parse(value)
         break;
       case ('set'):
+        if (!data) console.warn(`useStorage requires data as the third argument when using "set" as the first argument.`)
         return await AsyncStorage.setItem(itemName, JSON.stringify(data))
         break;
       case ('remove'):
         return await AsyncStorage.removeItem(itemName)
         break;
-      default:
-        return console.error('Invalid method')
     }
 
   } catch (error) {
