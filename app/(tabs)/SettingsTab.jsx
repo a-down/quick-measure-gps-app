44b300a85e0de617f3a3ec403668a895b1be21b1
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-26 13:05:18 -0600

    added test bottom-sheet to settings page

--- app/(tabs)/SettingsTab.jsx
+++ app/(tabs)/SettingsTab.jsx
@@ -1,10 +1,69 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { useRef, useMemo, useCallback } from 'react';
+import BottomSheet from '@gorhom/bottom-sheet';
 
 const Settings = () => {
+  const bottomSheetRef = useRef();
+  const snapPoints = useMemo(() => ['10%', '50%', '100%'], []);
+  const handleSheetChanges = useCallback((index) => {
+    console.log('handleSheetChanges', index)
+  }, []);
 
   return (
-    <>
-    </>
+    <View className="flex-1">
+      <BottomSheet
+      style={{ flex: 1 }}
+        ref={bottomSheetRef}
+        index={1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+      >
+        <ScrollView className="flex-1 p-4">
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+          <Text className="font-semibold">View/Edit Markers</Text>
+        </ScrollView>
+      </BottomSheet>
+
+    </View>
   )
 }
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,10 @@
 import { Redirect } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default () => {
-  return <Redirect href="/ToolsTab" />
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Redirect href="/ToolsTab" />
+    </GestureHandlerRootView>
+  )
 }
