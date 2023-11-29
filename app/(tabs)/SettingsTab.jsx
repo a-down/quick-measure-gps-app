092c005274d4e78c565abc8bb3a863cde8fdf225
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-29 13:16:31 -0600

    addition to settings menu

--- app/(tabs)/SettingsTab.jsx
+++ app/(tabs)/SettingsTab.jsx
@@ -1,20 +1,33 @@
-import { View, Text, ScrollView, Button } from 'react-native';
-import { useRef, useMemo, useCallback } from 'react';
-import BottomSheet from '@gorhom/bottom-sheet';
+import { View, Text, ScrollView, Button, Pressable } from 'react-native';
+import { useRef, useMemo, useCallback, useState, useEffect } from 'react';
 import { useStorage } from '../../hooks';
 
 const Settings = () => {
-  const bottomSheetRef = useRef();
-  const snapPoints = useMemo(() => ['10%', '50%', '100%'], []);
-  const handleSheetChanges = useCallback((index) => {
-    console.log('handleSheetChanges', index)
-  }, []);
+  const [ settingsState, setSettingsState ] = useState({})
+  
+  useEffect(() => {
+    const getSettings = async () => {
+      const map = await useStorage('get', 'mapPreferences')
+      const measurements = await useStorage('get', 'measurementPreferences')
+      console.log({map, measurements})
+      setSettingsState({map, measurements})
+    }
+    getSettings()
+  }, [])
 
   return (
-    <View className="flex-1">
-      <Button title="Delete Stored Data" onPress={() => {
+    <View className="flex-1 p-4">
+      {/* <Button title="Delete Stored Data" onPress={() => {
         useStorage('remove', 'savedMaps')
-      }} />
+      }} /> */}
+
+      <View className="flex-1" style={{gap: 8}}>
+        <Text className="text-lg font-semibold">App Preferences</Text>
+        <View>
+          <Text className="font-medium">Map Type</Text>
+
+        </View>
+      </View>
 
     </View>
   )
