82cd2cb6ca512da69748f1835843eb54e4fa02f0
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-30 21:04:36 -0600

    updated pressable sizing

--- app/(tabs)/MeasureTab.jsx
+++ app/(tabs)/MeasureTab.jsx
@@ -8,6 +8,7 @@ import { MaterialCommunityIcons, Ionicons, FontAwesome5, Feather } from '@expo/v
 export default function App() {
   const { width } = useWindowDimensions();
   const router = useRouter();
+  console.log(width)
 
   const data = [
     {link: '/AutoMeasureScreen', image: mapScreenshot, title: 'Auto', description: 'Measure automatically with GPS', icon: 'satellite-dish'},
@@ -24,14 +25,14 @@ export default function App() {
       case 'plus-circle':
         // return <Ionicons name={name} size={82} color='#8CC185' style={{textAlign: 'center', backgroundColor: '#ddd'}}/>
         // return <Feather name={name} size={82} color='#8CC185' style={{textAlign: 'center', marginBottom: 4}}/>
-        return <MaterialCommunityIcons name={name} size={82} color='#8CC185' style={{textAlign: 'center', marginBottom: 4}}/>
+        return <FontAwesome5 name={name} size={82} color='#8CC185' style={{textAlign: 'center', marginBottom: 4}}/>
 
     }
   }
 
   return (
     <View className="bg-green-9 relative flex-1 items-center">
-      <View className="bg-green-8 w-[1060px] aspect-square relative -top-[600px] rounded-full"></View>
+      <View className="bg-green-8 w-[1060px] aspect-square relative bottom-[600px] rounded-full"></View>
 
       <ScrollView className="flex-1 top-0 left-0 absolute p-8" contentContainerStyle={{ alignItems: 'center'}}>
         <View className="items-center justify-start" style={{gap: 24}}>
@@ -39,9 +40,9 @@ export default function App() {
           <Text className="text-white text-center mb-6" style={[bold, {fontSize: 24, maxWidth: 200}]}>Easy Tools for a Quick Measure</Text>
         </View>
 
-        <View className="bg-gray-1 w-full rounded-lg shadow-2xl p-4 flex-row flex-wrap justify-around" style={{width: width-64, gap: 16 }}>
+        <View className="bg-gray-1 w-full rounded-lg shadow-2xl p-4 flex-row flex-wrap justify-around" style={{width: width-64, gap: 16}}>
           {data.map((item, index) => (
-            <Pressable onPress={() => router.push(item.link)} className="items-center justify-start active:opacity-80" key={index} style={{maxWidth: 135}}>
+            <Pressable onPress={() => router.push(item.link)} className="items-center justify-end active:opacity-80" key={index} style={{width: 131}}>
               {Icon(item.icon)}
               <Text className=" text-green-10 text-center mt-2" style={[semibold, {fontSize: 18}]}>{item.title}</Text>
               <Text className=" text-gray-7 text-center" style={[regular, {fontSize: 14}]}>{item.description}</Text>  
