0d7fc5a3458da118f262c5c5aab12cb5bda5ef23
a-down <136649391+a2downing@users.noreply.github.com>
2023-12-01 10:52:09 -0600

    updated console.warn message

--- hooks/useStorage.js
+++ hooks/useStorage.js
@@ -1,9 +1,10 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
+// This hook is used to access AsyncStorage. It takes three arguments:
 export default useStorage = async (method, itemName, data) => {
   if (method !== "set" && method !== "get" && method !== "remove") console.warn(`"${method}" is not a valid argument for useStorage. Please use "get", "set", or "remove" as the first argument.`)
 
-  if (!method) console.warn(`useStorage requires a method as the first argument. Please use "get", "set", or "remove" as the first argument.`)
+  if (!method) console.warn(`useStorage requires a method and item name as arguments. Please use "get", "set", or "remove" as the first argument and an item name as the second.`)
 
   if (!itemName) console.warn(`useStorage requires an itemName. Please include the key of the item you wish to access as the second argument.`)
 
