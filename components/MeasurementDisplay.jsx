86a6d6bbeacfa0045c0a5452287c97e0138932c7
a-down <136649391+a2downing@users.noreply.github.com>
2023-12-03 20:05:19 -0600

    added hectares as area unit

--- components/MeasurementDisplay.jsx
+++ components/MeasurementDisplay.jsx
@@ -39,9 +39,10 @@ const MeasurementDisplay = ({ polygonArea, polygonDistance, setMapType, preferre
       [
         { text: "Sq Feet", onPress: () => storePreferences({...measurementPreferences, area: 'sq feet', areaShort: 'sqft'}) },
         { text: "Sq Yards", onPress: () => storePreferences({...measurementPreferences, area: 'sq yards', areaShort: 'sqyd'}) },
-        { text: "Sq Miles", onPress: () => storePreferences({...measurementPreferences, area: 'sq miles', areaShort: 'sqmi'}) },
         { text: "Acres", onPress: () => storePreferences({...measurementPreferences, area: 'acres', areaShort: 'a'}) },
+        { text: "Sq Miles", onPress: () => storePreferences({...measurementPreferences, area: 'sq miles', areaShort: 'sqmi'}) },
         { text: "Sq Meters", onPress: () => storePreferences({...measurementPreferences, area: 'sq meters', areaShort: 'sqm'}) },
+        { text: "Hectares", onPress: () => storePreferences({...measurementPreferences, area: 'hectares', areaShort: 'ha'}) },
         { text: "Sq Kilometers", onPress: () => storePreferences({...measurementPreferences, area: 'sq km', areaShort: 'sqkm'}) },
         { text: "Cancel", style: "cancel" }
       ]
