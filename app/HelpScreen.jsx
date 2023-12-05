06c8c9c021987a4c1f6f6d04ff0dd41c402758ae
a-down <136649391+a2downing@users.noreply.github.com>
2023-12-04 22:58:01 -0600

    updated with accents for action items

--- app/HelpScreen.jsx
+++ app/HelpScreen.jsx
@@ -1,6 +1,7 @@
 import { SafeAreaView, ScrollView, View, Text, Linking, Pressable } from 'react-native'
 import { regular, semibold, medium } from '../hooks/useJostFont'
 import React from 'react'
+import { Feather } from '@expo/vector-icons';
 
 const HelpScreen = () => {
   return (
@@ -10,21 +11,51 @@ const HelpScreen = () => {
           <Text style={[semibold, {fontSize: 20}]}>Usage Directions</Text>
 
           <Text style={[regular, {lineHeight: 28}]}>1. Choose a measurement tool (Auto, Manual, Tap).</Text>
-          <Text style={[regular, {lineHeight: 28}]}>2. "Start Measuring" in Auto Measure or start adding markers in Manual Measure or Tap to Measure.</Text>
-          <Text style={[regular, {lineHeight: 28}]}>3. Adjust display and measurement settings with the gear icon in the upper right of the screen.</Text>
-          <Text style={[regular, {lineHeight: 28}]}>4. Make changes with "Delete Options" to either select individual markers to delete or delete all markers.</Text>
+
+          <Text style={[regular, {lineHeight: 28}]}>2. 
+            <Text className="text-green-5" style={[semibold]}>
+              <Feather name='play' size={16} />{' Start Measuring '}
+            </Text>
+            in Auto Measure or start adding markers in Manual Measure or Tap to Measure.
+          </Text>
+
+          <Text style={[regular, {lineHeight: 28}]}>3. Adjust display and measurement settings with the
+            <Text className="text-green-5" style={[semibold]}>
+              {' gear icon '}<Feather name="settings" size={16}/>{' '}
+            </Text>
+            in the upper right of the screen.
+          </Text>
+
+          <Text style={[regular, {lineHeight: 28}]}>4. 
+            Make changes with
+            <Text className="text-green-5" style={[semibold]}>{' '}
+              <Feather name="trash-2" size={16}/>{' Delete Options '}
+            </Text>
+            to either select individual markers to delete or delete all markers.
+          </Text>
 
           <Text style={[regular, {lineHeight: 28}]}>
             5. Use 
-            <Text className="text-green-5" style={[semibold]}>Save</Text> 
-            to open the Save Menu. Then name your map and press "Save Map".
+            <Text className="text-green-5" style={[semibold]}>
+              {' '}<Feather name="download" size={16}/>{' Save '}
+            </Text> 
+            to open the Save Menu. Then name your map and press 
+            <Text className="text-green-5" style={[semibold]}>{' '}
+              <Feather name="download" size={16}/>{' Save Map'}
+            </Text>.
           </Text>
 
           <Text style={[regular, {lineHeight: 28}]}>6. View your saved measurement by returning to the home screen and navigating to the saved tab.</Text>
-          <Text style={[regular, {lineHeight: 28}]}>7. Click on a saved measurement to view the map, or delete the measurement with the trash icon.</Text>
+
+          <Text style={[regular, {lineHeight: 28}]}>
+            7. Click on a saved measurement to view the map, or delete the measurement with the
+            <Text className="text-green-5" style={[semibold]}>
+              {' trash icon '}<Feather name="trash-2" size={16}/>
+            </Text>.
+          </Text>
         </View>
 
-        <Text style={[medium, {lineHeight: 28}]}>Measurements are saved to your device. Measurements are NOT saved outside of your device (ex. online databse).</Text>
+        <Text style={[medium, {lineHeight: 28}]}>Measurements are saved to your device. Measurements are NOT saved outside of your device (ex. online database).</Text>
 
         <View style={{gap: 8}}>
           <Text style={[semibold, {fontSize: 20}]}>General Information</Text>
@@ -48,7 +79,7 @@ const HelpScreen = () => {
           </Text>
 
           <Text style={[regular, {lineHeight: 28}]}>If you love the app, I'd love to hear from you too. Please rate and review, and thank you for using Quick Measure!</Text>
-          <Text style={[semibold, {lineHeight: 28, textAlign: 'right'}]}>- Alec</Text>
+          <Text style={[semibold, {lineHeight: 28, textAlign: 'right', marginRight: 12}]}>- Alec</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
