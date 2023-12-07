1ce20f276d4198682bcc57a643419a8a759115b0
a-down <136649391+a2downing@users.noreply.github.com>
2023-12-07 08:42:23 -0600

    configured eas update

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,27 @@
 import { Redirect } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as Updates from 'expo-updates';
+import { Alert } from 'react-native';
+import { useEffect } from 'react';
 
 export default () => {
+  useEffect(() => {
+    onFetchUpdateAsync()
+  }, [])
+
+  const onFetchUpdateAsync = async () => {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      Alert.alert('Error fetching latest Expo update', error);
+      if (__DEV__) console.log(error)
+    }
+  }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
