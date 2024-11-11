7b578eba8fea7dc672f70185b5b4fdae31fda50d
a-down <alec@characterstrong.com>
2024-11-10 21:19:10 -0600

    added rules of react to eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,19 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "plugin:jsdoc/recommended"],
+  extends: [
+    "expo",
+    "plugin:jsdoc/recommended",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+  ],
   plugins: ["jsdoc"],
   rules: {
     "jsdoc/require-property-description": "off",
     "jsdoc/require-param-description": "off",
     "jsdoc/require-returns-description": "off",
     "jsdoc/tag-lines": "off",
+    "react/prop-types": "off",
   },
 };
